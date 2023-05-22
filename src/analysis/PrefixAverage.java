a9193b4ca6872b44e0a434ffb42d83ecafa7f83d
ghoshanirban <39007826+ghoshanirban@users.noreply.github.com>
2023-05-22 14:53:20 -0400

    Mon May 22 14:46:48 EDT 2023

--- src/analysis/PrefixAverage.java
+++ src/analysis/PrefixAverage.java
@@ -36,25 +36,28 @@ public class PrefixAverage {
 //		double[] x = {10, 0, 1, 5, 99, 3};
 //		System.out.println( Arrays.toString( findPrefixAveragesA(x)) );
 //		System.out.println( Arrays.toString( findPrefixAveragesB(x)) );
-		
+
 		Random generator = new Random();
 		int n = 200000;
 		double[] testArray = new double[n];
 		for( int i = 0; i < n; i++)
 			testArray[i] = generator.nextInt(n);
-		
-	    long startA = System.currentTimeMillis();
-	    findPrefixAveragesA(testArray);
-	    long timeTakenA = System.currentTimeMillis() - startA;
-	    
-	    System.out.println("Time taken by approach A (runs in O(n^2) time): " + timeTakenA + " ms" );
-	    
-	    long startB = System.currentTimeMillis();
-	    findPrefixAveragesB(testArray);
-	    long timeTakenB = System.currentTimeMillis() - startB;
-	    
-	    System.out.println("Time taken by approach B (runs in O(n) time): " + timeTakenB + " ms" );
-	    
-	    System.out.println("Speedup: " + (double)timeTakenA/timeTakenB);
+
+		long startA = System.currentTimeMillis();
+		var resultA = findPrefixAveragesA(testArray);
+		long timeTakenA = System.currentTimeMillis() - startA;
+
+		System.out.println("Time taken by approach A (runs in O(n^2) time): " + timeTakenA + " ms" );
+
+		long startB = System.currentTimeMillis();
+		var resultB = findPrefixAveragesB(testArray);
+		long timeTakenB = System.currentTimeMillis() - startB;
+
+		if( !Arrays.equals(resultA,resultB) )
+			throw new RuntimeException("Issues detected with the implementations.");
+
+		System.out.println("Time taken by approach B (runs in O(n) time): " + timeTakenB + " ms" );
+
+		System.out.println("Speedup: " + (double)timeTakenA/timeTakenB);
 	}
 }
