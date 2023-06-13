1ab2d2a10148582423756e93c293f0f10a439f3d
ghoshanirban <39007826+ghoshanirban@users.noreply.github.com>
2023-06-12 22:26:24 -0400

    Mon Jun 12 22:20:30 EDT 2023

--- src/tester/TestTrie.java
+++ src/tester/TestTrie.java
@@ -10,22 +10,23 @@ public class TestTrie {
 
         // source: https://github.com/dwyl/english-words, contains 370,104 words
         dictionary.loadWordsFromFile("datasets/words_alpha.txt");
+        System.out.println( "Number of words in the trie: " + dictionary.numberOfWords() );
+        System.out.println("Number of words in the trie: " + dictionary.numberOfNodes());
 
         ArrayList<String> words = new ArrayList<>();
         dictionary.wordsHavingPrefix("superab",words);
 
-        for(String s : words )
+        for( var s : words )
             System.out.print(s + " ");
 
         System.out.println( "\n" + dictionary.isPresent("abdomen") );
         System.out.println( "Number of words in the trie: " + dictionary.numberOfWords() );
 
         dictionary.removeWord("abdomen");
+        dictionary.removeWord("zodiac");
+
         System.out.println( "Number of words in the trie: " + dictionary.numberOfWords() );
 
         System.out.println( dictionary.isPresent("abdomen"));
-
-        System.out.println(dictionary.numberOfNodes());
-
     }
 }
