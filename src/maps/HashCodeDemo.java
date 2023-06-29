a6c2dbc63b103092914be457fb5e1ad65f17de5d
ghoshanirban <39007826+ghoshanirban@users.noreply.github.com>
2023-06-28 20:52:16 -0400

    Wed Jun 28 19:39:31 EDT 2023

--- src/maps/HashCodeDemo.java
+++ src/maps/HashCodeDemo.java
@@ -1,4 +1,4 @@
-package hashing;
+package maps;
 
 import introduction.Point;
 
@@ -11,14 +11,14 @@ public class HashCodeDemo {
 		System.out.println(s2.hashCode() + " ");
 		System.out.println(s3.hashCode());
 
-		Double d1 = 101.98;
-		System.out.print(d1.hashCode() + " ");
+		double d1 = 101.98;
+		System.out.print(Double.hashCode(d1) + " ");
 
 		d1 = 101.981;
-		System.out.print(d1.hashCode() + " ");
+		System.out.print(Double.hashCode(d1) + " ");
 
 		d1 = -101.981;
-		System.out.print(d1.hashCode() + " ");
+		System.out.print(Double.hashCode(d1) + " ");
 
 		Point p = new Point(1.0,2.0);
 		Point q = new Point(1.0,2.0);
